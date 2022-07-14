885295b2a1da77a2a40dff2d71047cc7468b06b9
XoutDragon <83075516+XoutDragon@users.noreply.github.com>
2022-07-14 18:19:17 -0400

    Create index.js

---
+++ src/projects/css/landkit-components-four/index.js
@@ -0,0 +1,35 @@
+const LandKitComponentsFour = () => {
+  return (
+    <div className='lg:pt-[6rem] pt-[10rem]'>
+      <div className='bg-[#f1f4f8] py-5 pl-5 w-fit'>
+        <div className='text-[1.7em] leading-10'>
+          <div>Stay focused on your business.</div>
+          <div className='text-[#345cec]'>
+            Let us handle the design<span className='text-black'>.</span>
+          </div>
+        </div>
+        <div className='text-[#536892] max-w-[28rem] text-[1.05em] pt-5 pb-8'>
+          You have a business to run. Stop worrying about cross-browser bugs,
+          designing new pages, keeping your components up to date. Let us do
+          that for you.
+        </div>
+        <div className='flex'>
+          <div className='flex-col'>
+            <div className='text-[1.1em]'>100%</div>
+            <div className='text-[#536892]'>Satisfaction</div>
+          </div>
+          <div className='flex-col px-9'>
+            <div className='text-[1.1em]'>24/ 7</div>
+            <div className='text-[#536892]'>Support</div>
+          </div>
+          <div className='flex-col'>
+            <div className='text-[1.1em]'>100k+</div>
+            <div className='text-[#536892]'>Sales</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandKitComponentsFour;
