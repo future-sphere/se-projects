cd0fefb0cd3c9fc72282af415c0a1a87c76b5b9a
XoutDragon <83075516+XoutDragon@users.noreply.github.com>
2022-07-14 18:17:59 -0400

    Create index.js

---
+++ src/projects/css/landkit-components-one/index.js
@@ -0,0 +1,26 @@
+const LandKitComponentsOne = () => {
+  return (
+    <div className='pb-7 bg-white w-fit pl-5 pr-[2.25rem] lg:pt-[6rem] pt-[10rem]'>
+      <div className='text-[2.4rem] max-w-[10em] leading-10 pb-4 pt-6 text-center md:text-left'>
+        Welcome to <span className='text-[#345cec]'>Landkit</span>. Develop
+        anything.
+      </div>
+      <div className='text-[#8a9cb9] text-[1.1rem] max-w-[24rem] font-medium text-center md:text-left'>
+        Build a beautiful, modern website with flexible Bootstrap components
+        built from scratch.
+      </div>
+      <div className='text-center pt-9 md:text-left'>
+        <button className='p-3 px-5 bg-[#345cec] text-[.8rem] text-white rounded-md'>
+          View all pages &nbsp; 🡪
+        </button>
+        <span className='pl-2'>
+          <button className='text-[#345cec] bg-[#eaeffd] text-[.8rem] px-4 rounded-md p-3 font-[600]'>
+            Documentation
+          </button>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default LandKitComponentsOne;
